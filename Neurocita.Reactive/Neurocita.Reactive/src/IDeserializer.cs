2f8d79481a757117748acad5761e33273cfdd144
Markus Drossel <mdr@alfapeople.com>
2021-11-02 19:45:42 +0100

    XML serialization

--- Neurocita.Reactive/Neurocita.Reactive/src/IDeserializer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.IO;
-
-namespace Neurocita.Reactive
-{
-    public interface IDeserializer
-    {
-        T Deserialize<T>(Stream stream);
-    }
-}
--- Neurocita.Reactive/Neurocita.Reactive/src/XmlSerializer.cs
+++ Neurocita.Reactive/Neurocita.Reactive/src/XmlSerializer.cs
@@ -4,6 +4,8 @@ namespace Neurocita.Reactive
 {
     public class XmlSerializer : ISerializer
     {
+        public string ContentType => "text/xml";
+
         public Stream Serialize<T>(T instance)
         {
             System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
@@ -11,6 +13,13 @@ namespace Neurocita.Reactive
             serializer.Serialize(stream, instance);
             stream.Position = 0;
             return stream;
+        }
+        
+        public T Deserialize<T>(Stream stream)
+        {
+            stream.Position = 0;
+            System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
+            return (T) serializer.Deserialize(stream);
         }
     }
 }
